d22dff81a3fcca6a3ae4060d3672707bbe4224a1
tirth707 <ptirth2206@gmail.com>
2026-06-03 08:26:28 +0530

    recursiion

--- recursion/myclass.java
+++ recursion/myclass.java
@@ -5,8 +5,15 @@ public class myclass {
         int n=2;
         int a=1;
 
-        f1(n);
+        // int arr[]={1,2};
+        // System.out.println(arr.length);
+        // f3(10);
+        // f1(n);
         // f2(n);
+        // f4(3);
+        // System.out.println(f4(10));
+        //  f5(10);
+        // System.out.println(f5(1110));
     }
 
     static void f1(int n){
@@ -28,5 +35,32 @@ public class myclass {
     f2(n-1);
     System.out.println(n);
 }
+
+
+static void f3(int n){
+    if(n==0){
+        return;
+    }
+    f3(n-1);
+    int total=0;
+    total+=n;
+    System.out.println(total);
+}
+
+//factorial of a  number 
+
+static int f4(int n){
+    if(n==1) return 1;
+    return n*f4(n-1);
+
+}
+
+//sum of digits of a number
+static int f5(int n){
+    if(n==1) return 1;
+    return n%10+f5(n/10);
+}
+
+
 
 }
